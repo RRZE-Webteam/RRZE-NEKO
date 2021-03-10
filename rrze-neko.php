34a8c41291136013445caf47777d1d6786962de1
Benjamin Klemencic <benjamin.klemencic@fau.de>
2021-03-10 10:11:44 +0100

    initial

---
+++ rrze-neko.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+Plugin Name:     RRZE NEKO
+Plugin URI:      https://github.com/RRZE-Webteam/RRZE-NEKO
+Description:     WordPress plugin with the famous NEKO cat >^.^<  
+Version:         0.1
+Author:          RRZE Webteam
+Author URI:      https://blogs.fau.de/webworking/
+License:         GNU General Public License v3
+License URI:     http://www.gnu.org/licenses/gpl-3.0.html
+Domain Path:     /languages
+Text Domain:     rrze-neko
+*/
+
+echo 'This is rrze-neko';
