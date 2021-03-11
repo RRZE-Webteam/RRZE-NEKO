101e4b72ff7b4c576413820cb1c8f38da1d5f1f8
Adrian Lindner <adrian.al.lindner@bs-erlangen.de>
2021-03-11 11:09:47 +0100

    Test mit Javascript

--- rrze-neko.php
+++ rrze-neko.php
@@ -4,7 +4,7 @@
 Plugin Name:     RRZE NEKO
 Plugin URI:      https://github.com/RRZE-Webteam/RRZE-NEKO
 Description:     WordPress plugin with the famous NEKO cat >^.^< Testtext 
-Version:         0.1.69
+Version:         0.1.70
 Author:          RRZE Webteam
 Author URI:      https://blogs.fau.de/webworking/
 License:         GNU General Public License v3
@@ -13,3 +13,8 @@ Domain Path:     /languages
 Text Domain:     rrze-neko
 */
 
+add_action('wp_enqueue_scripts','neko_init');
+
+function neko_init() {
+    wp_enqueue_script( 'n20171213.js', plugins_url( 'https://webneko.net/n20171213.js', __FILE__ ));
+}
