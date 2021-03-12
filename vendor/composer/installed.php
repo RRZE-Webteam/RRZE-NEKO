8c928b61d53c89edc9a664d83e8bbdbd61f1f888
Adrian Lindner <adrian.al.lindner@bs-erlangen.de>
2021-03-12 14:36:12 +0100

    Vendor added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '52367d7ff7cc23023cbd7a61909290d4f6d6af13',
+    'name' => 'neko/neko-basis',
+  ),
+  'versions' => 
+  array (
+    'neko/neko-basis' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '52367d7ff7cc23023cbd7a61909290d4f6d6af13',
+    ),
+  ),
+);
