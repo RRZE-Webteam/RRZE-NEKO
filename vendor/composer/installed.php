1ee9c2c7a5552c9632bececa8adddee9c6fc4417
Adrian Lindner <adrian.al.lindner@bs-erlangen.de>
2021-03-12 10:56:28 +0100

    Test w/ composer + submodule cmb2

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '80c6488dfd0e077fe9d512c53220c5fbce5e9368',
+    'name' => 'neko/neko-basis',
+  ),
+  'versions' => 
+  array (
+    'cmb2/cmb2' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cacbc8cedbfdf8ffe0e840858e6860f9333c33f2',
+    ),
+    'neko/neko-basis' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '80c6488dfd0e077fe9d512c53220c5fbce5e9368',
+    ),
+  ),
+);
